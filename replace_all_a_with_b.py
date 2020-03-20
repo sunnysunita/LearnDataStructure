b73d285b8c383f35cb77586b340b53644fe6d453
sunnysunita <sunnysunita.com@gmail.com>
2020-03-20 12:35:27 +0530

    final.

--- replace_all_a_with_b.py
+++ replace_all_a_with_b.py
@@ -1,15 +1,17 @@
 user_input=input("enter string: ")
 list=list(user_input)
+x=input("enter character to replace: ")
+y=input("enter replacing character: ")
 print(list)
 
-def rep_a_with_b(list,si):
+def rep_x_with_y(list,si,x,y):
     l=len(list)
     if si==l:
         return
-    if list[si]=="a":
-        list[si]="b"
-    return rep_a_with_b(list,si+1)
+    if list[si]==x:
+        list[si]=y
+    return rep_x_with_y(list,si+1,x,y)
 
 
-rep_a_with_b(list,0)
+rep_x_with_y(list,0,x,y)
 print(list)
